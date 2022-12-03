49a06337422ebbc84b2241bc36154c5408177d15
its-pikesh <pikesh.11606271@lpu.in>
2022-12-03 15:58:19 +0530

    Profile component added

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,18 +1,17 @@
-import react from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-import App from './App'
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <React.StrictMode>
+  <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>
-
-)
+);
