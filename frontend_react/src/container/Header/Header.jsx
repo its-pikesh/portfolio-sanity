9685b76d043ceb6216d2ba9e5404cbb41012b4d2
its-pikesh <pikesh.11606271@lpu.in>
2022-12-06 15:11:30 +0530

    About Skills comp added

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -17,7 +17,7 @@ const scaleVariants = {
 };
 
 const Header = () => (
-  <div className="app__header app__flex">
+  <div id="Home" className="app__header app__flex">
     <motion.div
       whileInView={{ x: [-100, 0], opacity: [0, 1] }}
       transition={{ duration: 0.5 }}
